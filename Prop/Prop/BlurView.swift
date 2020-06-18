20313e98e7e6c1122c11eab022b9988fd81d7a1b
Jonathan Kim <jonathankim@Jonathans-MBP-2.socal.rr.com>
2020-06-17 17:04:45 -0700

    implemented basic login view UI

---
+++ Prop/Prop/BlurView.swift
@@ -0,0 +1,34 @@
+//
+//  BlurView.swift
+//  Prop
+//
+//  Created by Jonathan Kim on 6/17/20.
+//  Copyright © 2020 nomadjonno. All rights reserved.
+//
+
+import SwiftUI
+
+struct BlurView: UIViewRepresentable {
+    typealias UIViewType = UIView
+
+    var style: UIBlurEffect.Style
+
+    func makeUIView(context: Context) -> UIView {
+        let view = UIView(frame: CGRect.zero)
+        view.backgroundColor = .clear
+
+        let blurEffect = UIBlurEffect(style: style)
+        let blurView = UIVisualEffectView(effect: blurEffect)
+        blurView.translatesAutoresizingMaskIntoConstraints = false
+        view.insertSubview(blurView, at: 0)
+
+        NSLayoutConstraint.activate([
+            blurView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            blurView.heightAnchor.constraint(equalTo: view.heightAnchor)
+        ])
+
+        return view
+    }
+
+    func updateUIView(_ uiView: UIView, context: Context) {}
+}
