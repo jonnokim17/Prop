8f747497f61f2d00fcf88c1a9434166ca842a578
Jonathan Kim <jonathankim@Jonathans-MBP-2.socal.rr.com>
2020-06-26 12:37:52 -0700

    updates to signup view with keyboard interaction

--- Prop/Prop/SignupView.swift
+++ Prop/Prop/SignupView.swift
@@ -15,14 +15,21 @@ struct SignupView: View {
     @State private var password = ""
     @State private var error = ""
     @State private var isLoading = false
+    @State var isFocused = false
 
     @EnvironmentObject var user: UserStore
-
     @Environment(\.presentationMode) private var presentationMode
+
+    private func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
 
     private func signUp() {
         self.isLoading = true
+        self.isFocused = false
+        self.hideKeyboard()
         Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
+            self.isLoading = false
             if let error = error {
                 self.error = error.localizedDescription
             } else {
@@ -53,57 +60,73 @@ struct SignupView: View {
 
     var body: some View {
         ZStack {
-            VStack {
-                Text("Create Account")
-                    .font(.system(size: 32, weight: .heavy))
-                Text("Sign up to get started")
-                    .font(.system(size: 18, weight: .medium))
-                    .foregroundColor(Color.gray)
-
-                VStack(spacing: 18) {
-                    TextField("Username", text: $username)
-                        .font(.system(size: 14))
-                        .padding(12)
-                        .background(RoundedRectangle(cornerRadius: 5).strokeBorder(Color.gray, lineWidth: 1))
-
-                    TextField("Email Address", text: $email)
-                        .keyboardType(.emailAddress)
-                        .font(.system(size: 14))
-                        .padding(12)
-                        .background(RoundedRectangle(cornerRadius: 5).strokeBorder(Color.gray, lineWidth: 1))
-
-                    SecureField("Password", text: $password)
-                        .font(.system(size: 14))
-                        .padding(12)
-                        .background(RoundedRectangle(cornerRadius: 5).strokeBorder(Color.gray, lineWidth: 1))
-                }
-                .padding(.vertical, 24)
-
-                Button(action: signUp) {
+            Color.white
+                .edgesIgnoringSafeArea(.all)
+            ZStack {
+                VStack {
                     Text("Create Account")
-                        .frame(minWidth: 0, maxWidth: .infinity)
-                        .frame(height: 50)
-                        .foregroundColor(.white)
-                        .font(.system(size: 14, weight: .bold))
-                        .background(Color.blue)
-                        .cornerRadius(5)
+                        .font(.system(size: 32, weight: .heavy))
+                    Text("Sign up to get started")
+                        .font(.system(size: 18, weight: .medium))
+                        .foregroundColor(Color.gray)
+
+                    VStack(spacing: 18) {
+                        TextField("Username", text: $username)
+                            .font(.system(size: 14))
+                            .padding(12)
+                            .background(RoundedRectangle(cornerRadius: 5).strokeBorder(Color.gray, lineWidth: 1))
+                            .onTapGesture {
+                                self.isFocused = true
+                        }
+                        TextField("Email Address", text: $email)
+                            .keyboardType(.emailAddress)
+                            .font(.system(size: 14))
+                            .padding(12)
+                            .background(RoundedRectangle(cornerRadius: 5).strokeBorder(Color.gray, lineWidth: 1))
+                            .onTapGesture {
+                                self.isFocused = true
+                        }
+                        SecureField("Password", text: $password)
+                            .font(.system(size: 14))
+                            .padding(12)
+                            .background(RoundedRectangle(cornerRadius: 5).strokeBorder(Color.gray, lineWidth: 1))
+                            .onTapGesture {
+                                self.isFocused = true
+                        }
+                    }
+                    .padding(.vertical, 24)
+
+                    Button(action: signUp) {
+                        Text("Create Account")
+                            .frame(minWidth: 0, maxWidth: .infinity)
+                            .frame(height: 50)
+                            .foregroundColor(.white)
+                            .font(.system(size: 14, weight: .bold))
+                            .background(Color.blue)
+                            .cornerRadius(5)
+                    }
+
+                    if !error.isEmpty {
+                        Text(error)
+                            .font(.system(size: 14, weight: .semibold))
+                            .foregroundColor(Color.red)
+                            .padding()
+                    }
+
+                    Spacer()
+                        .padding(.horizontal, 32)
                 }
+                .offset(y: -40)
 
-                if !error.isEmpty {
-                    Text(error)
-                        .font(.system(size: 14, weight: .semibold))
-                        .foregroundColor(Color.red)
-                        .padding()
+                if isLoading {
+                    LoadingView()
                 }
-
-                Spacer()
-                    .padding(.horizontal, 32)
             }
-            .offset(y: -40)
-
-            if isLoading {
-                LoadingView()
-            }
+            .offset(y: isFocused && screen.height < 700 ? -60 : 0)
+        }
+        .onTapGesture {
+            self.isFocused = false
+            self.hideKeyboard()
         }
     }
 }
